d0e19f89f9660b32051e001309b7b9bbdc704fa1
elrin <jaehun.j.kim@gmail.com>
2020-04-17 23:45:42 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def readme():
@@ -25,7 +25,8 @@ setup(name='aarms',
       author='Jaehun Kim',
       author_email='j.h.kim@tudelft.nl',
       license='MIT',
-      packages=['aarms'],
+      # packages=['aarms'],
+      packages=find_packages('.')
       install_requires=requirements(),
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3'],
